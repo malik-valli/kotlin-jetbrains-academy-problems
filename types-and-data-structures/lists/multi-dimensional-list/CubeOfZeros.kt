63ab0aed49cc607b7b788a9cb8b8b862114590c9
Malik <81878781+malik-valli@users.noreply.github.com>
2022-07-12 23:23:21 +0500

    Create CubeOfZeros.kt

---
+++ types-and-data-structures/lists/multi-dimensional-list/CubeOfZeros.kt
@@ -0,0 +1,6 @@
+// Link to the problem - hyperskill.org/learn/step/15135
+
+fun main() {
+    val matrix3d = Array(3) { Array(3) { Array(3) { 0 } } }
+    println(matrix3d.contentDeepToString()) // [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+}
